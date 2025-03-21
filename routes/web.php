412ab1b3cb64be8f87b867e214207940271e66c2
Darren <tanjunnyi1107@hotmail.com>
2025-03-21 22:10:28 +0800

    added product

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('/dashboard');
+    return view('home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/admin', function () {
+    return view('admin.product.index');
+})->middleware('auth')->name('admin');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('products', ProductController::class)->middleware('auth');
 
 require __DIR__ . '/auth.php';
